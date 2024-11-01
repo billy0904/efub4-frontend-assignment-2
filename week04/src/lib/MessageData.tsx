c86709343df5063261cfef2a3d6744531ba9e252
Gabin Lee <gb94@ewhain.net>
2024-11-01 17:32:46 +0900

    Fix: 메시지 데이터 수정

--- week04/src/lib/MessageData.tsx
+++ week04/src/lib/MessageData.tsx
@@ -7,7 +7,7 @@ export const MessageData = [
     },
     {
         senderId: 0,
-        text: "4주차 recoil 과제 제출 마감일이 궁금합니다!",
+        text: "recoil 과제 제출 마감일이 궁금합니다!",
         timestamp: new Date('2024-09-25T10:30:00'),
         read: true,
     },
@@ -19,7 +19,7 @@ export const MessageData = [
     },
     {
         senderId: 1,
-        text: "EFUB 노션 페이지에 관련 공지 업로드되어 있습니다. 참고해주세요!",
+        text: "EFUB 노션 일정 페이지에 4주차 과제 관련 공지를 업로드 해두었습니다. 참고해주세요!",
         timestamp: new Date('2024-09-25T10:35:00'),
         read: true,
     },
